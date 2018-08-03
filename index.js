da3c292f7077a24eaa0f1e4bcc000551b9636f4b
Nirmal Gupta <nirmalk.gupta@gmail.com>
2018-08-03 13:53:09 -0700

    exposing nonce

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Rest   = require('./src/Rest');
 const Socket = require('./src/Socket');
+const nonce  = require('./src/nonce')
 
 module.exports = function zka(origin, apiPath) {
-  const zka  = {};
-  const baseUrl    = origin + apiPath;
-  zka.rest   = Rest(baseUrl)
-  zka.socket = Socket(origin)
+  const zka     = {};
+  const baseUrl = origin + apiPath;
+  zka.rest      = Rest(baseUrl)
+  zka.socket    = Socket(origin)
+  zka.nonce     = nonce
 
   zka.init = function (accountId, apiKey, secret) {
     zka.account = {id: accountId, apiKey, secret}
